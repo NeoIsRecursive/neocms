506c6399f4901527426746397069ff7b83a5a7be
NeoIsRecursive <64473191+NeoIsRecursive@users.noreply.github.com>
2022-03-24 08:37:51 +0100

    Add support for emoji

--- resources/views/post/all.blade.php
+++ resources/views/post/all.blade.php
@@ -3,12 +3,12 @@
 <div class="container-fluid">
     @foreach ($posts as $post)
     <div class="container-fluid">
-        <h2>{{ $post->slug }}</h2>
+        <h2>{{ $post->title }}</h2>
         <p>author: {{ $post->author }}</p>
         <p>published: {{ $post->created_at }}</p>
         <a href="/post/{{ $post->slug }}">read more</a>
     </div>
-        @endforeach
+    @endforeach
     {{$posts->links()}}
 </div>
 
--- resources/views/post/new.blade.php
+++ resources/views/post/new.blade.php
@@ -1,4 +1,5 @@
-<div style="display: grid; grid-template-columns:1fr 1fr; height:100vh; width:100vw;">
+@include('components.head')
+<div style="display: grid; grid-template-columns:1fr 1fr; height:100vh; width:100vw; max-width:100vw;">
     <form action="/new_post" method="post">
         @csrf
         <textarea name="body" id="input" style="height: 100%; width:100%"></textarea>
@@ -8,3 +9,8 @@
 
     </div>
 </div>
+@production
+@else
+<script type="module" src="http://localhost:3000/@vite/client"></script>
+<script type="module" src="http://localhost:3000/resources/scripts/main.js"></script>
+@endproduction
